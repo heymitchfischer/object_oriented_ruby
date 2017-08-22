9fc4d2316b0be5337cc4344e4ab99e8fd027f2d0
heymitchfischer <mitchfischer6@gmail.com>
2017-08-21 21:26:23 -0500

    made some employee.rb edits

--- employee.rb
+++ employee.rb
@@ -18,8 +18,16 @@ class Employee
     @active = input_active
   end
 
+  def first_name
+    return @first_name
+  end
+
   def print_info
     puts "#{@first_name} #{@last_name} makes #{@salary} a year."
+  end
+
+  def give_annual_raise
+    @salary = @salary * 1.05
   end
 end
 
@@ -27,5 +35,8 @@ end
 
 employee_1 = Employee.new("Jimmy", "Stewart", 70000, true)
 employee_2 = Employee.new("Susan", "Messing", 80000, true)
-employee_1.print_info
+
 employee_2.print_info
+employee_2.give_annual_raise
+employee_2.print_info
+p employee_2.first_name
